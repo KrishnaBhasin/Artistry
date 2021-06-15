844b388a5978b8a34d558c2641c1cb0132a473b9
KrishnaBhasin <Krishna.Bhasin@outlook.com>
2021-06-15 19:15:04 +0530

    Changes Done for sorting and pagination

---
+++ Core/Specification/ProductWithFilterForCountSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    public class ProductWithFilterForCountSpecification:BaseSpecification<Product>
+    {
+        public ProductWithFilterForCountSpecification(ProductsSpecParams productsSpecParams) : base(x =>
+                  (string.IsNullOrEmpty(productsSpecParams.Search) || x.Name.ToLower()
+                    .Contains(productsSpecParams.Search))&&
+                  (!productsSpecParams.BrandId.HasValue || x.ProductBrandId == productsSpecParams.BrandId) && //filteration
+                  (!productsSpecParams.TypeId.HasValue || x.ProductTypeId == productsSpecParams.TypeId)
+            )
+        {
+
+        }
+    }
+}
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -17,8 +17,22 @@ namespace Infrastructure.Data
             var query = inputQuery;
             if(spec.Criteria!=null)
             {
-                query = inputQuery.Where(spec.Criteria);
+                query = query.Where(spec.Criteria);
             }
+            if(spec.OrderBy!=null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+            if(spec.OrderByDesc!=null)
+            {
+                query = query.OrderByDescending(spec.OrderByDesc);
+            }
+
+            if(spec.IsPagingEnable)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
