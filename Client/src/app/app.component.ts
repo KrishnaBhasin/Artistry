758b30cc1f3c7472a45b2508a08cff9cb589eaad
KrishnaBhasin <Krishna.Bhasin@outlook.com>
2021-06-19 20:52:52 +0530

    Changes for lint file

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
+import { HttpClient } from '@angular/common/http';
+import { Component ,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Artistry';
+  products: IProduct[];
+
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+
+    this.http.get('https://localhost:44357/api/Products?pageSize=50').subscribe((response:IPagination)=>{
+      this.products=response.data;
+    },(error:IPagination)=>{
+      console.log(error);
+    })
+
+  }
+
 }
